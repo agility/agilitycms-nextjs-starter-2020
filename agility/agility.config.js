6d2c53fc16b35808880091b881a928854238d569
azure_pipelines <support@agilitycms.com>
2020-08-20 14:26:29 -0400

    change to keep cache in /tmp/ folder

--- agility/agility.config.js
+++ agility/agility.config.js
@@ -15,7 +15,7 @@ const getSyncClient = ({ isPreview, isDevelopmentMode }) => {
 
 	let cachePath = `node_modules/@agility/content-sync/cache/${isPreview ? 'preview' : 'live'}`
 
-	if (!isDevelopmentMode && isPreview) {
+	if (!isDevelopmentMode) {
 		//we are in a lambda function, need to use /tmp
 		cachePath = "/tmp/agilitycache"
 	}
