0f85e3db26a2b5887fce3648f27cfe0b89218bdb
Daniel Karpienia <dkarpienia@hotmail.com>
2020-08-21 08:56:30 -0400

    Update [...slug].js

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,35 +1,30 @@
-import React, { Component } from 'react';
-import { getAgilityPageProps, getAgilityPaths } from 'agility/agility.node'
-import { handlePreview } from 'agility/agility.browser'
-import Layout from 'components/agility-global/Layout'
+import React, { Component } from "react";
+import { getAgilityPageProps, getAgilityPaths } from "agility/agility.node";
+import { handlePreview } from "agility/agility.browser";
+import Layout from "components/agility-global/Layout";
 
 class AgilityPage extends Component {
-	render() {
-		handlePreview();
+  render() {
+    handlePreview();
 
-		return (
-			<Layout {...this.props} />
-		)
-	}
+    return <Layout {...this.props} />;
+  }
 }
 
 export async function getStaticProps(context) {
-
-	const props = await getAgilityPageProps({ context });
-	return {
-		props: props,
-		revalidate: 1 //will refresh page every 1 second on Vercel
-	}
-
+  const props = await getAgilityPageProps({ context });
+  return {
+    props: props,
+    revalidate: 1, //will refresh page every 1 second on Vercel...
+  };
 }
 
 export async function getStaticPaths() {
-	const paths = await getAgilityPaths();
-	return {
-		paths: paths,
-		fallback: true
-	}
+  const paths = await getAgilityPaths();
+  return {
+    paths: paths,
+    fallback: true,
+  };
 }
 
-export default AgilityPage
-
+export default AgilityPage;
