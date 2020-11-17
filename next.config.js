b0c90792fbf48638cbbd1d16eebc18534325e4bc
Joshua Isaac <hello.joshua@hotmail.com>
2020-11-17 15:45:58 -0500

    updated to react 17 & next 10, added next image

--- next.config.js
+++ next.config.js
@@ -1,21 +1,26 @@
 module.exports = {
-	webpack: (config, { isServer }) => {
-	  // Fixes npm packages that depend on `fs` module
-	  if (!isServer) {
-		config.node = {
-		  fs: 'empty',
-		  module: "empty"
-		}
-	  }
+  // Agility CDN domain config for Next/Image
+  images: {
+    domains: [`${process.env.AGILITY_GUID}-cdn.agilitycms.cloud`],
+  },
 
-	  config.module.rules.push({
-		test: /\.svg$/,
-		issuer: {
-		  test: /\.(js|ts)x?$/,
-		},
-		use: ['@svgr/webpack', 'url-loader'],
-	  });
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+        module: "empty",
+      };
+    }
 
-	  return config
-	}
-  }
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ["@svgr/webpack", "url-loader"],
+    });
+
+    return config;
+  },
+};
