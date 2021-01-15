8f1fd26b7fb7f865daa5c2bc3e9bf6b3cc751799
Joshua Isaac <hello.joshua@hotmail.com>
2021-01-15 15:04:41 -0500

    updated SEO component

---
+++ components/seo/SEO.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Head from "next/head";
+import ReactHtmlParser from "react-html-parser";
+
+const SEO = ({ title, description, keywords, ogImage, metaHTML }) => {
+  // setup and parse additional header markup
+  let additionalHeaderMarkup = null;
+  if (metaHTML) {
+    additionalHeaderMarkup = ReactHtmlParser(metaHTML);
+  }
+  return (
+    <Head>
+      <title>{title} - Agility CMS Sample Blog</title>
+      <meta name="generator" content="Agility CMS" />
+      <meta name="agility_timestamp" content={new Date().toLocaleString()} />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      {ogImage && <meta property="og:image" content={ogImage} />}
+      {additionalHeaderMarkup}
+      <link rel="stylesheet" href="/prose.css" />
+    </Head>
+  );
+};
+
+export default SEO;
