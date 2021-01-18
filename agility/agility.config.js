d8cbc7f713524ef9c70e59228c918fedbafe6c00
Joel Varty <joel@agilitycms.com>
2021-01-18 14:55:49 -0500

    updated sync logic

--- agility/agility.config.js
+++ agility/agility.config.js
@@ -1,51 +1,78 @@
 const agilityContentSync = require("@agility/content-sync");
 const agilityFileSystem = require("@agility/content-sync/src/store-interface-filesystem");
 
+const path = require("path")
+const fs = require('fs-extra');
+
 const agilityConfig = {
-  guid: process.env.AGILITY_GUID, //Set your guid here
-  fetchAPIKey: process.env.AGILITY_API_FETCH_KEY, //Set your fetch apikey here
-  previewAPIKey: process.env.AGILITY_API_PREVIEW_KEY, //set your preview apikey
-  languageCode: "en-us", //the language for your website in Agility CMS
-  channelName: "website", //the name of your channel in Agility CMS
-  securityKey: process.env.AGILITY_SECURITY_KEY, //the website security key used to validate and generate preview keys
+	guid: process.env.AGILITY_GUID, //Set your guid here
+	fetchAPIKey: process.env.AGILITY_API_FETCH_KEY, //Set your fetch apikey here
+	previewAPIKey: process.env.AGILITY_API_PREVIEW_KEY, //set your preview apikey
+	languageCode: "en-us", //the language for your website in Agility CMS
+	channelName: "website", //the name of your channel in Agility CMS
+	securityKey: process.env.AGILITY_SECURITY_KEY, //the website security key used to validate and generate preview keys
 };
 
-const getSyncClient = ({ isPreview, isDevelopmentMode }) => {
-  let cachePath = `node_modules/@agility/content-sync/cache/${
-    isPreview ? "preview" : "live"
-  }`;
+const getSyncClient = ({ isPreview, isDevelopmentMode, isIncremental }) => {
 
-  if (!isDevelopmentMode) {
-    //we are in prod and need to use a different directory that Vercel understands
-    cachePath = `/tmp/agilitycache/${isPreview ? "preview" : "live"}`;
-  }
+	const rootPath = process.cwd()
 
-  console.log(`Agility CMS => Content cache path is ${cachePath}`);
-  const apiKey = isPreview
-    ? agilityConfig.previewAPIKey
-    : agilityConfig.fetchAPIKey;
+	let cachePath = `${rootPath}/.next/cache/agility/${agilityConfig.guid}/${isPreview ? "preview" : "live" }`;
 
-  if (!agilityConfig.guid) {
-    console.log("Agility CMS => No GUID was provided.");
-    return null;
-  }
+	//if we are in "incremental" mode, we need to use the tmp folder...
+	if (isIncremental) {
+		//TODO: this is the tmp folder for vercel...  if running in a regular container, the cache path can stay the same...
+		cachePath = `/tmp/agilitycache/${agilityConfig.guid}/${isPreview ? "preview" : "live"}`;
+	}
 
-  return agilityContentSync.getSyncClient({
-    guid: agilityConfig.guid,
-    apiKey: apiKey,
-    isPreview: isPreview,
-    languages: [agilityConfig.languageCode],
-    channels: [agilityConfig.channelName],
-    store: {
-      interface: agilityFileSystem,
-      options: {
-        rootPath: cachePath,
-      },
-    },
-  });
+	console.log(`AgilityCMS => Content cache path is ${cachePath}`);
+	const apiKey = isPreview
+		? agilityConfig.previewAPIKey
+		: agilityConfig.fetchAPIKey;
+
+	if (!agilityConfig.guid) {
+		console.log("AgilityCMS => No GUID was provided.");
+		return null;
+	}
+
+	return agilityContentSync.getSyncClient({
+		guid: agilityConfig.guid,
+		apiKey: apiKey,
+		isPreview: isPreview,
+		languages: [agilityConfig.languageCode],
+		channels: [agilityConfig.channelName],
+		store: {
+			interface: agilityFileSystem,
+			options: {
+				rootPath: cachePath
+			},
+		},
+	});
 };
+
+
+const prepIncrementalMode = async () => {
+
+	const rootPath = process.cwd()
+	let cachePath = `${rootPath}/.next/cache/agility/`;
+	const tempPath = `/tmp/agilitycache/`;
+
+	const buildFilePath = path.join(tempPath, "build.log")
+
+	//check for the build file in here...
+	if (!fs.existsSync(buildFilePath)) {
+		console.log(`Copying Agility Content files to temp folder: ${tempPath}`)
+		//copy everything across from cachePath
+		await fs.copy(cachePath, tempPath)
+	} else {
+		console.log(`Agility tmp folder up to date: ${tempPath}`)
+	}
+}
+
+
 
 module.exports = {
-  agilityConfig,
-  getSyncClient,
+	agilityConfig,
+	getSyncClient,
+	prepIncrementalMode
 };
